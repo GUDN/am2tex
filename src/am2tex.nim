098097b5a7c6d791961af1e79c805a6aa8a46dd8
Daniil Novoselov <gudnmail@gmail.com>
2020-11-17 08:28:33 +0500

    clean a project

--- src/am2tex.nim
+++ src/am2tex.nim
@@ -1,7 +1,2 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
-import am2texpkg/submodule
-
 when isMainModule:
-  echo(getWelcomeMessage())
+  echo "Hello, World!"
